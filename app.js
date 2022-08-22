60dd1ab39f8e7ba3c03329ce95059bd670bcc5ba
Shivani Soni <eng.shivanisoni2405@gmail.com>
2022-08-22 22:20:17 +0530

    update html,css

---
+++ app.js
@@ -0,0 +1,16 @@
+var txtInput=document.querySelector("#input");
+var btntranslate=document.querySelector("#btn-translate");
+var outputtxt=document.querySelector("#output");
+var serrverURL="https://api.funtranslations.com/translate/sindarin.json";
+
+function getTranslation(text)
+{
+    return serrverURL +"? "+"text= "+text;
+}
+
+function errorHandler(error)
+{
+    
+}
+
+
