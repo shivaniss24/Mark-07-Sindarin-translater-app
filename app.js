91fea79225cb98795f9af503202607abb3233883
Shivani Soni <eng.shivanisoni2405@gmail.com>
2022-08-23 14:38:38 +0530

    update js,html

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-var txtInput=document.querySelector("#input");
-var btntranslate=document.querySelector("#btn-translate");
-var outputtxt=document.querySelector("#output");
-var serrverURL="https://api.funtranslations.com/translate/sindarin.json";
+var input=document.querySelector("#input-ctn");
+var btn=document.querySelector("#btn-translate");
+var output=document.querySelector("#output-ctn");
+var serverURL="https://api.funtranslations.com/translate/sindarin.json"
 
 function getTranslation(text)
 {
-    return serrverURL +"? "+"text= "+text;
+    return serverURL +"? "+"text= "+text;
 }
 
 function errorHandler(error)
 {
-         
+  console.log("error occured",error);
+  alert("something went wrong with server!.Try again")  
 }
+function convertText()
+{
+    var inputtext=input.value;
+    if(inputtext==""){
+        alert("Please enter some text");
+        return;
+    }
 
+    // const url=`${serverURL}?text=${inputtext}`;
+    //taking input value
+    
+    //calling server for processing 
+    fetch(getTranslation(inputtext))
+    .then(response=>response.json())
+    .then(json=>{
+        var outputText=json.contents.translated;
+        output.innerText=outputText;
+    })
+    .catch(errorHandler)
+};
+btn.addEventListener("click",convertText);
 
